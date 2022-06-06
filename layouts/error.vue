6815023534089f00c179c065662eb43aba5cfd48
gkNorth <mgk.webcreate@gmail.com>
2022-06-07 08:03:26 +0900

    エラーページ追加

---
+++ layouts/error.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{error.statusCode}}</h1>
+    <p>{{error.message}}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+
+@Component({
+  layout: 'error'
+})
+export default class Error extends Vue {
+  @Prop()
+  error!: object
+}
+</script>
+
+<style>
+
+</style>
